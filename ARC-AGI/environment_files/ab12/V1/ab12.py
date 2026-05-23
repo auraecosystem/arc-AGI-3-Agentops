b6248df4fdc75d91c15ff03efeb37fe6f9bd983c
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-05-23 01:32:16 +0100

    Create ab12.py

---
+++ ARC-AGI/environment_files/ab12/V1/ab12.py
@@ -0,0 +1,6 @@
+ARC-AGI/
+└── environment_files/
+    └── ab12/               # use any game ID you want
+        └── v1/             # use any version identifier you want
+            ├── ab12.py     # must match game ID above
+            └── metadata.json
